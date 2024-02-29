973e9f3f7bb69c48e0bcdf10cbbd525bdca74c98
crashdemons <crashenator@gmail.com>
2024-02-28 23:06:27 -0600

    massive update: import,export,copy,continuous color fade mode, different command support, OOP-rewrite, Shift-click to enter hex codes

--- script.js
+++ script.js
@@ -1,81 +1,307 @@
-function docReady(fn) {
-    // see if DOM is already available
-    if (document.readyState === "complete" || document.readyState === "interactive") {
-        // call on next available tick
-        setTimeout(fn, 1);
-    } else {
-        document.addEventListener("DOMContentLoaded", fn);
+
+class NickFade {
+    static UI = class {
+        static docReady(fn) {
+            // see if DOM is already available
+            if (document.readyState === "complete" || document.readyState === "interactive") {
+                // call on next available tick
+                setTimeout(fn, 1);
+            } else {
+                document.addEventListener("DOMContentLoaded", fn);
+            }
+        }
+
+        /**
+         * @param {HTMLElement|Document} parent
+         * @returns {Element[]}
+         */
+        static getColorBoxes = (parent=document) => Array.from(parent.getElementsByClassName('i'));
+        static getSections = () => Array.from(document.getElementsByTagName('li'));
+        static getText = (li)=>Array.from((li.getElementsByClassName('input')[0].value || ''));
+        static getTextStr = (li)=>(li.getElementsByClassName('input')[0].value || '');
+        static clearColors(){ this.getColorBoxes().forEach(i => i.value = '#ffffff'); }
+        static debugColors(){  this.getColorBoxes().forEach(span => {console.log(span.value)}); }
+        /** @param {HTMLElement|Document} parent */
+        static getColors = (parent=document) => this.getColorBoxes(parent)
+            .map(colorbox => colorbox.value.substring(1)).map(hex => NickFade.Util.toRGB(hex));
+        static getColorsHex = (parent=document) => this.getColorBoxes(parent)
+            .map(colorbox => colorbox.value);
+
+        static getFlags(){
+            let flags=[];
+            let checkFlags =['italic','underline','bold','strikethrough'];
+            for(let check of checkFlags){
+                let state = document.getElementById(check).checked || false;
+                if(state) flags.push(check);
+            }
+            return flags;
+        }
+        static getCommandType(){
+            return document.getElementById('command-type').value || 'essentials';
+        }
+
+        static copyCommand(){
+            let command = document.getElementById('command');
+            //command.select();
+            //command.setSelectionRange(0,999999);//mobile
+            navigator.clipboard.writeText(command.innerText);
+            alert("Copied text!");
+        }
+        static isContinuous(){
+            return document.getElementById('continuouscolor').checked ?? true;
+        }
+        static applyContinuousColorMode(){
+            let state = this.isContinuous();
+            let boxes = this.getColorBoxes();
+            //console.log(state);
+            let lastvalue = null;
+            for(let i=boxes.length-1;i>=0;i--){
+                if(i%2===1){
+                    if(state){
+                        boxes[i].classList.add('disabled');
+                        if(lastvalue!==null) boxes[i].value = lastvalue;
+                    }
+                    else boxes[i].classList.remove('disabled');
+                }
+                lastvalue = boxes[i].value || "";
+            }
+            for(let box of boxes){
+
+            }
+        }
+        static getTexts(){
+            return this.getSections().map(li=>this.getTextStr(li))
+        }
+
+        static getState(){
+            return {
+                colors: this.getColorsHex(),
+                texts: this.getTexts(),
+                continuouscolor: this.isContinuous()?1:0,
+                flags: this.getFlags(),
+                commandType: this.getCommandType()
+            }
+        }
+        static setState(state){
+            let boxes = this.getColorBoxes();
+            let sections = this.getSections();
+            for(let i=0;i<boxes.length;i++){
+                boxes[i].value = state.colors[i]??'';
+            }
+            for(let i=0;i<sections.length;i++){
+                sections[i].getElementsByTagName('input').value=state.texts[i];
+            }
+            document.getElementById('continuouscolor').checked = !!(state.continuouscolor??0);
+            document.getElementById('command-type').value = state.commandType??"essentials";
+            let checkFlags =['italic','underline','bold','strikethrough'];
+            for(let check of checkFlags){
+                document.getElementById(check).checked = (state.flags??[]).includes(check);
+            }
+
+        }
+
+        static exportState(){
+            let state=this.getState();
+            let json = JSON.stringify(state);
+            let datauri = 'data:application/json;base64,'+btoa(json);
+            let link = document.createElement('a');
+
+            let time = (new Date()).toISOString().substring(0,19);"2024-02-29T04:41:15"
+            time = time.replaceAll(':','-').replace('T','_');
+
+            link.href=datauri;
+            link.download=encodeURIComponent(this.getTexts().join(''))+"_"+time+".nickfade";
+            link.click();
+        }
+        static importStateJSON(json){
+            let state = JSON.parse(json);
+            this.setState(state);
+        }
+        static importState(){
+            let input = document.createElement('input');
+            input.type="file";
+            input.accept=".nickfade"
+
+            input.onchange=function(event){
+                if(event.target.files.length===0) return;
+                var fr=new FileReader();
+                fr.onload=function(){
+                    NickFade.UI.importStateJSON(fr.result);
+                };
+                fr.readAsText(event.target.files[0]);
+            }
+
+            input.click();
+        }
+
+        static promptHexColorForInput(input,evt){
+            let hex = prompt("Please input a hex color code (6 digits A-F). Example: FF00FF");
+            if(hex===null || hex===false || hex===undefined || hex==="") return;
+            if(!/^[0-9a-fA-F]{6}$/.test(hex)){
+                alert("Invalid hex color!");
+                return;
+            }
+            input.value = "#"+hex;
+        }
     }
-}
+    static Util = class {
+        static blend = ([a1, a2], r) => a1.map((p, i) => Math.round(p * (1 - r) + a2[i] * r));
+        static toHex = rgb => rgb.map(n => n.toString(16).padStart(2, '0')).join('');
+        static toRGB = hex => hex.match(/([0-9a-fA-F]{2})/g).map(hexbyte => Number.parseInt(hexbyte, 16));
+    }
 
-docReady(function () {
-
-
-    const reg = {
-        '000000': '0',
-        '0000AA': '1',
-        '00AA00': '2',
-        '00AAAA': '3',
-        'AA0000': '4',
-        'AA00AA': '5',
-        'FFAA00': '6',
-        'AAAAAA': '7',
-        '555555': '8',
-        '5555FF': '9',
-        '55FF55': 'a',
-        '55FFFF': 'b',
-        'FF5555': 'c',
-        'FF55FF': 'd',
-        'FFFF55': 'e',
-        'FFFFFF': 'f'
-    };
-    const blend = ([a1, a2], r) => a1.map((p, i) => Math.round(p * (1 - r) + a2[i] * r));
-    const toHex = rgb => rgb.map(n => n.toString(16).padStart(2, '0')).join('');
-    const toRGB = hex => hex.match(/([0-9a-fA-F]{2})/g).map(hexbyte => Number.parseInt(hexbyte, 16));
-    Array.from(document.getElementsByClassName('i')).forEach(i => i.value = '#ffffff');
-    setInterval(() => {
-        let command = '', preview = '', last;
-        Array.from(document.getElementsByTagName('li')).forEach(li => {
-            const colors = Array.from(li.getElementsByClassName('i'))
-                    .map(span => span.value.substring(1)).map(hex => toRGB(hex));
-            /*.map(rgb => Array.from(rgb.matchAll(/\d+/g)).map(([c]) => Number.parseInt(c)));*/
-            const text = Array.from((li.getElementsByClassName('input')[0].value || '').trim());
-            const italic = document.getElementsByClassName('italic')[0].checked;
-            const styl = (italic)?"&o":"";
-            const fstyl = (italic)?"italic":"normal";
-            
-            
-            const coloredText = text.map((c, idx) => {
+    static Formats = class {
+        static vanillaColors = {
+            '000000': '0',
+            '0000AA': '1',
+            '00AA00': '2',
+            '00AAAA': '3',
+            'AA0000': '4',
+            'AA00AA': '5',
+            'FFAA00': '6',
+            'AAAAAA': '7',
+            '555555': '8',
+            '5555FF': '9',
+            '55FF55': 'a',
+            '55FFFF': 'b',
+            'FF5555': 'c',
+            'FF55FF': 'd',
+            'FFFF55': 'e',
+            'FFFFFF': 'f'
+        };
+        static vanillaFormats={
+            'obfuscated':'k',
+            'bold':'l',
+            'strikethrough':'m',
+            'underline':'n',
+            'italic':'o',
+            'reset':'r',
+        };
+        static flagsToEssentials(flags,prefixChar='&'){
+            let formats = "";
+            for(let flag of flags) formats += `${prefixChar}${this.vanillaFormats[flag]}`;
+            return formats;
+        }
+        static flagsToCSS(flags){
+            let fontweight="normal"
+            let textdecoration=[];
+            let fontstyle="normal";
+            if(flags.includes('bold')) fontweight="bold";
+            if(flags.includes('strikethrough')) textdecoration.push("line-through");
+            if(flags.includes('underline')) textdecoration.push("underline");
+            if(flags.includes('italic')) fontstyle="italic";
+            textdecoration=textdecoration.join(' ');
+            return `font-weight:${fontweight}; text-decoration:${textdecoration}; font-style:${fontstyle};`
+        }
+        static fromString(text, blendColors, flags){
+            return text.map((c, idx) => {
                 const pct = idx / Math.max(1, text.length - 1);
-                return {c, rgb: blend(colors, pct)};
+                return {c, rgb: NickFade.Util.blend(blendColors, pct), flags : flags};
             });
-            command += coloredText.map(o => {
-                const hex = toHex(o.rgb).toLowerCase()
-                const curr = reg[hex] || '#' + hex;
-                const ret = curr == last ? o.c : `&${curr}${styl}${o.c}`;
-                last = curr;
-                return ret;
-            }).join('');
+        }
 
-            preview += coloredText.map(o => `<span style="color:#${toHex(o.rgb)}; font-style:${fstyl};">${o.c}</span>`).join('');
+        static toEssentialsColor(hexcolor,prefixChar='&',expandedForm=false,forceHex=false){
+            let vanillaCode = this.vanillaColors[hexcolor.toUpperCase()] ?? null;
+            if(forceHex || vanillaCode===null){
+                if(expandedForm){
+                    let nibbles = Array.from(hexcolor);
+                    let code = `${prefixChar}x`
+                    for(let nibble of nibbles){
+                        code += `${prefixChar}${nibble}`;
+                    }
+                    return code;
+                }else{
+                    return `${prefixChar}#${hexcolor}`
+                }
+            }else{
+                return `${prefixChar}${vanillaCode}`
+            }
+        }
+        static toEssentials(formats, prefixChar='&',expandedForm=false,forceHex=false){
+            let lastColor = null;
+            let lastStyle = null;
+            return formats.map(o => {
+                const hex = NickFade.Util.toHex(o.rgb);
+                let essStyle = NickFade.Formats.flagsToEssentials(o.flags,prefixChar);
+                let essColor = this.toEssentialsColor(hex,prefixChar,expandedForm,forceHex);
+
+                let colorChanged = lastColor!==essColor
+                let styleChanged = lastStyle!==essStyle;
+                lastColor=essColor;
+                lastStyle=essStyle;
+                //console.log(colorChanged,lastColor,essColor,(!colorChanged && !styleChanged),(!colorChanged && styleChanged))
+                if(!colorChanged && !styleChanged){ essColor=""; essStyle=""; }//if nothing changed, use no formatting
+                else if(!colorChanged && styleChanged){ essColor=""; essStyle=`${prefixChar}r`+essStyle; }//if only the style changed, just reapply the style
+                //if the color changed, then we have to reapply everything because colors reset formatting in MC.
+                return `${essColor}${essStyle}${o.c}`;
+            }).join('');
+        }
+
+        static toCommand(formats,type){
+            switch(type){
+                case 'essentials': return this.toEssentials(formats,'&',false);
+                case 'essentials-raw': return this.toEssentials(formats,"\xA7",true);
+                case 'nick': return "/nick "+this.toEssentials(formats,'&',false);
+                case 'itemname': return "/iname "+this.toEssentials(formats,'&',false);
+                case 'itemlore': return "/ilore "+this.toEssentials(formats,'&',false);
+                case 'lorehand': return "/lore "+this.toEssentials(formats,'&',false).replaceAll(' ','_');
+            }
+        }
+
+        static toPreview(formats){
+            return formats.map(o =>{
+                let css = this.flagsToCSS(o.flags);
+                return `<span style="color:#${NickFade.Util.toHex(o.rgb)}; ${css}">${o.c}</span>`
+            }).join('');
+        }
+    }
+
+
+
+
+
+    static process(){
+        let command = '', preview = '', last;
+        this.UI.applyContinuousColorMode();
+
+        this.UI.getSections().forEach(li => {
+            const colors = this.UI.getColors(li);
+            /*.map(rgb => Array.from(rgb.matchAll(/\d+/g)).map(([c]) => Number.parseInt(c)));*/
+            const text = this.UI.getText(li);
+            const flags = this.UI.getFlags();
+            const commandType = this.UI.getCommandType();
+            //console.log('flags',flags)
+
+
+            const coloredText = this.Formats.fromString(text,colors,flags);
+            command += this.Formats.toCommand(coloredText,commandType);
+            preview += this.Formats.toPreview(coloredText);
         });
         if (document.getElementById('command').innerText !== command) {
             document.getElementById('command').innerText = command;
             document.getElementById('preview').innerHTML = preview;
         }
-    }, 1000)
+    }
+    static init(){
+        this.UI.docReady( ()=>{
+            this.UI.clearColors();
+            document.getElementById('copy').onclick = this.UI.copyCommand.bind(this.UI);
+            document.getElementById('export').onclick = this.UI.exportState.bind(this.UI);
+            document.getElementById('import').onclick = this.UI.importState.bind(this.UI);
+            this.UI.getColorBoxes().forEach(box=>{
+                box.onclick=function(e){
+                    if (e.shiftKey || e.altKey || e.ctrlKey){
+                        NickFade.UI.promptHexColorForInput(this,e);
+                        e.preventDefault();
+                    }
+                }
+            });
 
-});
-
-
-
-function debugColors() {
-
-    Array.from(document.getElementsByTagName('li')).forEach(li => {
-        const colors = Array.from(li.getElementsByClassName('i'))
-                .forEach(span => {
-                    console.log(span.value)
-                });
-    });
-
+            this.process();
+            setInterval(this.process.bind(this), 1000)
+        });
+    }
 }
+
+
+NickFade.init();
